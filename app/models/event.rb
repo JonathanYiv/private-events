0e4856c4eb908f1542093780cfb5612a667dc89a
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-14 22:33:31 -0400

    set up associations for attendances

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ApplicationRecord
 
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
+  has_many :attendances
+  has_many :attendees, through: :attendances, source: :user
+
   validates :title, presence: true, length: { minimum: 6, maximum: 140 }
   validates :description, presence: true, length: { minimum: 40, maximum: 1500 }
   validates :start_time, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
 
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
